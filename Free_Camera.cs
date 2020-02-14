ddd7a7634dfbea01515b0968aeeba95c26fb169c
Dylan Tai <dylztai@gmail.com>
2020-02-13 16:06:06 -0800

    Push

---
+++ Free_Camera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Free_Camera : MonoBehaviour
+{
+    [SerializeField] GameObject obj_camera;
+    [SerializeField] GameObject obj_free_camera;
+    private bool enabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enabled = false;
+        obj_free_camera.SetActive(false);
+        obj_camera.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            if (enabled)
+            {
+                enabled = false;
+                obj_free_camera.SetActive(false);
+                obj_camera.SetActive(true);
+            }
+            else
+            {
+                enabled = true;
+                obj_free_camera.SetActive(true);
+                obj_camera.SetActive(false);
+            }
+        }
+        if (enabled)
+        {
+            if (Input.GetKey("up"))
+                this.transform.position = new Vector3(this.transform.position.x + 1, this.transform.position.y, this.transform.position.z);
+            if (Input.GetKey("down"))
+                this.transform.position = new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z);
+            if (Input.GetKey("left"))
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1);
+            if (Input.GetKey("right"))
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+        }
+    }
+}
---
+++ Pivot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour
+{
+    public float rotate_speed;
+    [SerializeField] GameObject obj_ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = obj_ball.transform.position;
+        this.transform.Rotate(0, -Input.GetAxis("Horizontal") * Time.deltaTime * rotate_speed, 0);
+    }
+}
